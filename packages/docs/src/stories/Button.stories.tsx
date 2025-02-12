b74260ddce121d5c9009599a7caa750a97c7eb04
ygor <ygorthiagoff@outlook.com>
2025-02-12 15:02:48 +0100

    setup storybook

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,17 @@
+import { Button, type ButtonProps } from '@rogy-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
